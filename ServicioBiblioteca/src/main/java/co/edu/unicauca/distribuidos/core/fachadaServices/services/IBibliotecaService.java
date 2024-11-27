681f8672c3562db1d58e42a57e53d6d3b8ab45fc
LauraSofi05 <laubotina@unicauca.edu.co>
2024-11-27 14:43:45 -0500

    Biblioteca

---
+++ ServicioBiblioteca/src/main/java/co/edu/unicauca/distribuidos/core/fachadaServices/services/IBibliotecaService.java
@@ -0,0 +1,20 @@
+
+package co.edu.unicauca.distribuidos.core.fachadaServices.services;
+
+import java.util.List;
+
+//import co.edu.unicauca.distribuidos.core.fachadaServices.DTO.ClienteDTO;
+import co.edu.unicauca.distribuidos.core.fachadaServices.DTO.BibliotecaPeticionDTO;
+import co.edu.unicauca.distribuidos.core.fachadaServices.DTO.BibliotecaRespuestaDTO;
+
+/*Se trabaja con un cliente DTO porque no se debe exponer los entitys al exterior
+ * Aqui en los DTO se define que informacion enviar
+ */
+public interface IBibliotecaService {
+
+	public List<BibliotecaRespuestaDTO> findAll();
+
+	public BibliotecaRespuestaDTO findById(Integer id);
+
+	public boolean delete(Integer id);
+}
